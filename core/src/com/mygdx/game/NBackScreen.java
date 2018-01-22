4ddf8df4b2bcc7bf2dc9d8c31a106847ffc3cfd8
Dziedzic <Micha Dziedzic>
2018-01-22 11:23:34 +0100

    HALO, ROBIE UPDATE

--- core/src/com/mygdx/game/NBackScreen.java
+++ core/src/com/mygdx/game/NBackScreen.java
@@ -8,11 +8,17 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 
+import java.util.LinkedList;
 import java.util.Random;
 
 
 public class NBackScreen implements Screen {
+    private final ImageButton positionButton;
+    private final ImageButton typeButton;
+    private final ImageButton positionAndTypeButton;
+    private final LinkedList<ImageButton> posList;
     Stage stage;
     final MainGame game;
     OrthographicCamera camera;
@@ -23,9 +29,30 @@ public class NBackScreen implements Screen {
         this.game = game;
         stage = new Stage();
         Assets.load();
-        belt = new Belt();
-        rndGen = new Random();
-        belt.firstTimeBelt(rndGen);
+
+
+        positionButton = new ImageButton(new TextureRegionDrawable(new TextureRegion(new Texture("core/assets/pola/pole_pos.png"))));
+        typeButton = new ImageButton(new TextureRegionDrawable(new TextureRegion(new Texture("core/assets/pola/pole_typ.png"))));
+        positionAndTypeButton = new ImageButton(new TextureRegionDrawable(new TextureRegion(new Texture("core/assets/pola/pole_both.png"))));
+
+
+        positionButton.setPosition(550,350);
+        typeButton.setPosition(550,200);
+        positionAndTypeButton.setPosition(550,50);
+        
+        posList = new LinkedList<ImageButton>();
+        
+        posList.add(positionButton);
+        posList.add(typeButton);
+        posList.add(positionAndTypeButton);
+
+        for(ImageButton i: posList){
+            i.setVisible(true);
+            i.setSize(200,100);
+            stage.addActor(i);
+        }
+
+
     }
 
     @Override
